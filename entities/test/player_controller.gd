fb8256994c2b02e2ea6b5552012a63baeffc7f19
Robert Moegenburg <r.moegenburg@t-online.de>
2025-12-27 20:50:49 +0100

    player_controller

--- entities/test/player_controller.gd
+++ entities/test/player_controller.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 
-const SPEED: float = 500
+const SPEED: float = 500.0
+const ACCELERATION : float = 1110.0
 
 var _input_vector : Vector2 = Vector2.ZERO
-
+var _old_velocity : Vector2  = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,12 +13,17 @@ func _input(_event: InputEvent) -> void:
 	_input_vector = Input.get_vector("move_left","move_right", "move_forward", "move_backward")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	
+	#_old_velocity = linear_velocity
+	
+	linear_velocity = _get_velocity(_delta)
 
-	move_and_collide(_get_velocity(delta))
 
-func _get_velocity(delta : float ) ->Vector2 : 
+func _get_velocity(_delta: float) ->Vector2 : 
+	var _new_velocity : Vector2 = _input_vector * SPEED
 
-	var _new_velocity : Vector2 = _input_vector * delta * SPEED
-
-	return _new_velocity
+	var _result : Vector2 = _old_velocity.move_toward(_new_velocity,ACCELERATION * _delta)
+	_old_velocity = _result
+	return _result
+	#return linear_velocity.move_toward(_new_velocity, ACCELERATION * _delta)
