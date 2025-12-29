adbd3291da35906dfbb12ebac7e2a49c09432589
Robert Moegenburg <r.moegenburg@t-online.de>
2025-12-29 10:55:12 +0100

    game_data and persistent

---
+++ autoload/game_data.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var current_upgrades : Dictionary = {
+    "upgrade_food_dense" : 0
+}
+var game_settings : Dictionary = {
+    "master_volume" : 0.5,
+    "music_volume" : 0.5,
+    "sound_volume" : 0.5,
+    "fullscreen" : 0,
+    "show_fps" : 1,
+    "max_bacterias" : 1000,
+    "save_game_data" : 1,
+    "save_interval" :60
+}
+var player_progress : Dictionary = {
+    "bacterias" : 1,
+    "petri_dishes" : 0, # total distance traveled in kilometers (astronomical units)
+    "player_archived_game_goal" : 0,
+    "upgrades": current_upgrades
+}
---
+++ autoload/persistent_manager.gd
@@ -0,0 +1,77 @@
+extends Node
+
+@onready var _timer : Timer = $Timer
+
+################################## Functions ##################################
+func _ready() -> void:
+    ### DEBUG 
+    #reset_game_data()
+    ### /DEBUG
+    _timer.timeout.connect(_on_timer_timeout)
+    _timer.start()
+    #Load existing game data if available
+    load_game_data()
+
+func _on_timer_timeout() -> void:
+    save_game_data()
+
+##################### DATA SAVE, LOAD AND RESET METHODS #####################
+func save_game_data() -> void:
+    var data_to_save : Dictionary = {
+        "game_settings" : GameData.game_settings,
+        "player_progress" : GameData.player_progress
+    }
+    # Create a ConfigFile to save the data
+    var config = ConfigFile.new()
+    #Store the data in the config file
+    config.set_value("game_data", "data", data_to_save)
+    # Save the config file to disk
+    var err = config.save("user://save_game.cfg")
+    if err != OK:
+        print("Error saving game data: ", err)
+    else :
+        print("Game data saved successfully.")
+    
+
+func load_game_data() -> void:
+    # Create a ConfigFile to load the data
+    var config = ConfigFile.new()
+
+    # Load the config file from disk
+    var err = config.load("user://save_game.cfg")
+    # If the file didn't load successfully, return
+    if err != OK:
+        print("No save file found, starting new game.")
+        return
+    # Retrieve the saved data
+    var saved_data = config.get_value("game_data", "data", null)
+    # If saved data exists, populate the variables
+    if saved_data != null:
+        GameData.game_settings = saved_data.get("game_settings", GameData.game_settings)
+        GameData.player_progress = saved_data.get("player_progress", GameData.player_progress)
+        print("Game data loaded successfully.")
+    else:
+        print("No saved data found in the save file.")
+
+func reset_game_data() -> void:
+    GameData.current_upgrades = {
+        "upgrade_food_dense" : 0
+    }
+    GameData.game_settings = {
+        "master_volume" : 0.5,
+        "music_volume" : 0.5,
+        "sound_volume" : 0.5,
+        "fullscreen" : 0,
+        "show_fps" : 1,
+        "max_bacterias" : 1000,
+        "save_game_data" : 1,
+        "save_interval" :60
+    }
+    GameData.player_progress = {
+        "bacterias" : 1,
+        "petri_dishes" : 0, # total distance traveled in kilometers (astronomical units)
+        "player_archived_game_goal" : 0,
+        "upgrades": GameData.current_upgrades
+}
+    # Save Reseted Data
+    save_game_data()
