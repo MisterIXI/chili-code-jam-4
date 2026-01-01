52ac4fbdf6dd6cc1e76086e9da144fa746a42ada
Robert Moegenburg <r.moegenburg@t-online.de>
2026-01-01 22:32:39 +0100

    food_manager_update

--- autoload/food_manager.gd
+++ autoload/food_manager.gd
@@ -6,7 +6,7 @@ var base_food_spawntime : float = 0.5
 var stat_food_cycles : int  = 0
 var offset : Vector2 = Vector2( 50,50)
 #privates
-const FOOD_MULT: int = 3
+const FOOD_MULT: int = 1
 @onready var timer : Timer = $Timer
 var food_cd: float = 0.5
 var food_per_sec: float = 10
@@ -15,19 +15,22 @@ func get_food_rate() -> float:
 	return timer.wait_time * FOOD_MULT
 
 func _ready() -> void:
-	# timer.timeout.connect(_on_timer_timeout)
+	timer.timeout.connect(_on_timer_timeout)
 
 	GameUi.food_slider_changed.connect(_on_food_interval_change)
-	GameUi._food_slider.min_value = 0
-	GameUi._food_slider.max_value = 100
-	GameUi._food_slider.value = 10
+	GameUi._food_slider.min_value = 01
+	GameUi._food_slider.max_value = 10
+	GameUi._food_slider.value = 5
+	GameUi._food_slider.step = 1
 
 func _on_timer_timeout() ->void:
+
 	next_cycle()
 
 func next_cycle() ->void:
 	# get camera
-	for x in range(FOOD_MULT):
+	#OLD for x in range(FOOD_MULT):
+	for x in range(GameData.u_food_drop_max + FOOD_MULT):
 		spawn((Vector2.RIGHT * randf() * 850).rotated(randf() * PI * 2))
 	# stat up
 	stat_food_cycles += 1
@@ -40,7 +43,7 @@ func spawn(_pos : Vector2) ->void:
 	#set values
 	_instance.food_amount = base_food_value * stat_food_cycles
 
-   
+
 func _on_food_interval_change(_value : float) ->void:
 	if _value > 0:
 		timer.wait_time = _value
